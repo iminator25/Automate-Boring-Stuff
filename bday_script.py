c20649defff90a70ab816b8a27e12d1d92f9635e
Imran Adamjee <43097446+iminator25@users.noreply.github.com>
2020-07-04 21:37:19 -0700

    Added browser opening

--- bday_script.py
+++ bday_script.py
@@ -1,4 +1,19 @@
 import schedule
 import time
+import webbrowser
 
-print('making birthday note script')
+def acecess_FB():
+	print('Its working')
+	url = 'https://www.facebook.com/'
+	chrome = webbrowser.get('chrome')
+	chrome.open_new('https://www.facebook.com/')
+	return
+
+acecess_FB()
+# schedule.every(1).minutes.do(acecess_FB)
+# schedule.every().day.at('20:00').do(acecess_FB)
+
+
+# while True:
+# 	schedule.run_pending()
+# 	time.sleep(1)
