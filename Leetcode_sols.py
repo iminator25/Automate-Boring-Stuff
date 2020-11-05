da9dc0a8711902c340b0cfdedd4f29226ca9e7d5
Imran Adamjee <43097446+iminator25@users.noreply.github.com>
2020-11-04 22:13:54 -0600

    two sum II

--- Leetcode_sols.py
+++ Leetcode_sols.py
@@ -274,7 +274,29 @@ class Solution:
         else:
             return True
 #-----------------------------------------====------------------------------------------
+# 167. Two Sum II - Input array is sorted
+
+# Given an array of integers that is already sorted in ascending order, 
+# find two numbers such that they add up to a specific target number.
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        low = 0
+        high = len(numbers)-1
+        
+        while low < high:
+            add = numbers[low] + numbers[high]
+            if add == target:
+                return (low+1, high+1)
+            elif (add > target):
+                high -= 1
+            elif (add < target):
+                low += 1
+            
+            
+
 
 
 #-----------------------------------------====------------------------------------------
         
+#-----------------------------------------====------------------------------------------
