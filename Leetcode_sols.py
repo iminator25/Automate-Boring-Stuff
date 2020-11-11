92f301c173933ec0c827ff9be16e9872f9c40258
Imran Adamjee <43097446+iminator25@users.noreply.github.com>
2020-11-11 13:59:24 -0600

    answered 2 more questions

--- Leetcode_sols.py
+++ Leetcode_sols.py
@@ -323,3 +323,19 @@ class Solution:
 
 
 #-----------------------------------------====------------------------------------------
+
+# 1323. Maximum 69 Number
+
+# Given a positive integer num consisting only of digits 6 and 9.
+# Return the maximum number you can get by changing at most one digit 
+# (6 becomes 9, and 9 becomes 6).
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        l = str(num)
+        l = l.replace('6','9',1)
+        l = int(l)
+        return l 
+
+
+#-----------------------------------------====------------------------------------------
